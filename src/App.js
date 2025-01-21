65cc06fc65618be9fa0962b44d7eceb29ea18cca
Sayan_2001 <103439037+Sayan62@users.noreply.github.com>
2025-01-22 02:53:02 +0530

    gsap integration
    Starting to use gsap...failed to do so

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
+import gsap from "gsap";
+
 import './App.css';
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
+gsap.registerPlugin(useGSAP); 
 
 function App() {
+  const container = useRef();
+  
+  useGSAP(
+    () => {
+      // use selectors...
+      gsap.to(".box", { rotation: "+=360", duration: 3 });
+
+      // or refs...
+      // gsap.to(circle.current, { rotation: "-=360", duration: 3 });
+    },
+    { scope: container }
+  );
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +28,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link box"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
